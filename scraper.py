daa77c78770c6810c1f4344c31a6b368f758b141
YumJelly <yumjelly666@gmail.com>
2023-11-07 15:19:02 +0800

    add css style to all chapter

--- scraper.py
+++ scraper.py
@@ -59,6 +59,8 @@ def scraper_for_quanben5(base_url: str, target_url: str, epub_folder_path: str, 
         book.add_author(author_name)
         book.add_metadata('DC', 'description', book_description)
 
+        
+
         book_cover_url = soup.find('div', class_='pic').find('img').get('src')
         if book_cover_url is not None and not replace_book_cover :
             print(book_cover_url)
@@ -75,6 +77,25 @@ def scraper_for_quanben5(base_url: str, target_url: str, epub_folder_path: str, 
             img.save(f"{epub_folder_path}{book_title}.jpg")
             book.set_cover(f"{book_title}.jpg", open(f"{epub_folder_path}{book_title}.jpg", 'rb').read())
             print("No book cover found or you want to replace it, So I created one for you.")
+        # Create the epub file
+        # define CSS style
+        # add default NCX and Nav file
+        book.add_item(epub.EpubNcx())
+        book.add_item(epub.EpubNav())
+
+        # add CSS file
+        style = 'p {padding: 1em;\
+                text-indent: 2.5em;\
+                line-height: 1.6em;\
+                word-break: break-all;\
+                text-align: justify;\
+            }'
+        nav_css = epub.EpubItem(
+            uid="style_nav",
+            file_name="style/nav.css",
+            media_type="text/css",
+            content=style,
+        )
         
         c = 0
         # Add chapters to the epub file
@@ -95,26 +116,27 @@ def scraper_for_quanben5(base_url: str, target_url: str, epub_folder_path: str, 
                 # Find the chapter content
                 # chapter_content = chapter_soup.find('div', id='content').get_text() # this will get the text only
                 chapter_lines = chapter_soup.find('div', id='content').find_all('p') # this will get the text with html tags
-                chapter_lines = str().join(str(line) for line in chapter_lines)
+                chapter_lines = '<h2>' + chapter_name + '</h2>' + str().join(str(line) for line in chapter_lines)
                 # print(chapter_lines)
                 # Create a chapter
                 chapter = epub.EpubHtml(title=chapter_name, file_name=f'chapter_{chapter_name}.xhtml', lang='zh')
                 chapter.content = chapter_lines
+                chapter.add_item(nav_css)
                 # Add the chapter to the book
                 book.add_item(chapter)
                 book.toc.append(chapter)
                 book.spine.append(chapter)
-        # Create the epub file
-        epub.write_epub(epub_folder_path + book_title + ".epub", book, {})
-    else:
-        print(f"Failed to retrieve the webpage: Status code {response.status_code}")
-    
-    
+            if c == 2:
+                break
 
+        book.add_item(nav_css)
+        # write to the file
+        epub.write_epub(epub_folder_path + book_title + ".epub", book, {})
+        
 
 if __name__ == '__main__':
     base_url = 'https://big5.quanben5.com'
     # change target_url to the book you want to download
     target_url = 'https://big5.quanben5.com/n/doushentianxia/xiaoshuo.html'
     epub_folder_path = './epub/'
-    scraper_for_quanben5(base_url, target_url, epub_folder_path)
+    scraper_for_quanben5(base_url, target_url, epub_folder_path, replace_book_cover=False)
